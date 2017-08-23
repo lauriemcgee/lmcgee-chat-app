37973f756b20c03dea7e9487a301abfece5d754e
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-08-22 19:25:47 -0500

    formatting changes

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -5,6 +5,7 @@ import { WebService } from '../web.service';
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.css'],
   providers: [ WebService ]
 })
 export class ChatsComponent {
